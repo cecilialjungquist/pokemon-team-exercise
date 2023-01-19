937f879c20c17b8476e0c95be745b53495eb5447
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-01-19 11:51:17 +0100

    Function for render out pokemons

--- app.js
+++ app.js
@@ -36,3 +36,35 @@ const pokemons = [
     }
 ];
 
+let pokemonsAvailable = [];
+let pokemonsChosen = [];
+
+pokemons.forEach(pokemon => {
+    let pokemonEl = document.createElement('article');
+
+    pokemonEl.innerHTML = `
+            <figure class="pokemon">
+            <img src="jay-dkFJST9zZZo-unsplash.jpg" alt="Pokemon ball.">
+            <figcaption>
+                <h4>${pokemon.name}</h4>
+                <p>${pokemon.cp}CP</p>
+            </figcaption>
+        </figure>
+    `;
+    pokemonsAvailable.push(pokemon);
+
+    pokemonEl.addEventListener('click', swap);
+    document.querySelector('.available').appendChild(pokemonEl);
+});
+
+function swap() {
+    console.log('clicked');
+    pokemonsChosen.push(this);
+    pokemonsAvailable.pop(this)
+    document.querySelector('.chosen').appendChild(this);
+    console.log(pokemonsAvailable);
+    console.log(pokemonsChosen);
+}
+
+// Om den finns i available ska den läggas till i chosen
+// Om den finns i chosen ska den läggas till i available
