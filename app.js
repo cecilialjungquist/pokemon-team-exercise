0ca4b3f564933e3c9e2b9794860a70e71f743662
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-01-19 10:45:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const pokemons = [
+    {
+        id: 1,
+        name: 'Bulbasaur',
+        cp: 439
+    },
+    {
+        id: 37,
+        name: 'Vulpix',
+        cp: 194
+    },
+    {
+        id: 134,
+        name: 'Vaporeon',
+        cp: 2469
+    },
+    {
+        id: 130,
+        name: 'Gyarados',
+        cp: 2406
+    },
+    {
+        id: 25,
+        name: 'Pikachu',
+        cp: 455
+    },
+    {
+        id: 27,
+        name: 'Sandshrew',
+        cp: 710
+    },
+    {
+        id: 52,
+        name: 'Meowth',
+        cp: 121
+    }
+];
+
